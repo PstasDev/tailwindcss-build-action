976f52c913cff63a5d2e9325fbf8093b10b13d14
Pstas <60182573+PstasDev@users.noreply.github.com>
2024-07-15 01:51:26 +0200

    chore: Update run.sh to add git authentication with token

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ npx tailwindcss --input "$INPUT_INPUT" --output "$INPUT_OUTPUT" "$INPUT_PARAMS"
 
 git config --global --add safe.directory /github/workspace
 git add --force src/output.css
+git auth --with-token "$INPUT_TOKEN"
 git config user.name "PstasDev"
 git config user.email "contact@botond.eu"
 git diff-index --quiet HEAD || git commit -sm "tailwindcss-update"
