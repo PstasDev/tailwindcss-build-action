1c59d264389b7b97c9c2dfadce5f151be8407357
Pstas <60182573+PstasDev@users.noreply.github.com>
2024-07-15 01:47:00 +0200

    chore: Update run.sh to add safe.directory configuration

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ npm install
 
 npx tailwindcss --input "$INPUT_INPUT" --output "$INPUT_OUTPUT" "$INPUT_PARAMS"
 
+git config --global --add safe.directory /github/workspace
 git add --force src/output.css
 git config user.name "PstasDev"
 git config user.email "noreply@botond.eu"
