47ae884060cc74784388487f60ce6602eeb8ec3b
Pstas <60182573+PstasDev@users.noreply.github.com>
2024-07-15 02:04:56 +0200

    chore: Update run.sh to use npx command for tailwindcss action

--- run.sh
+++ run.sh
@@ -5,11 +5,3 @@ npm install -D tailwindcss
 npm install
 
 npx tailwindcss --input "$INPUT_INPUT" --output "$INPUT_OUTPUT" "$INPUT_PARAMS"
-
-git config --global --add safe.directory /github/workspace
-git add --force $INPUT_OUTPUT
-git auth --with-token "$INPUT_TOKEN"
-git config user.name "PstasDev"
-git config user.email "contact@botond.eu"
-git diff-index --quiet HEAD || git commit -sm "tailwindcss-update"
-git push
